fe906a325a7dbd4946704f1158691d978abaf80c
hiatus169 <2968447202@qq.com>
2025-05-31 22:55:59 +0800

    feat: DebugUtil支持打印成员变量
    Signed-off-by: hiatus169 <2968447202@qq.com>

--- core/src/main/java/im/mingxi/debug/DebugUtil.kt
+++ core/src/main/java/im/mingxi/debug/DebugUtil.kt
@@ -3,8 +3,19 @@ package im.mingxi.debug
 import im.mingxi.loader.bridge.XPBridge
 
 object DebugUtil {
+
     fun printAllFieldStatic(clz: Class<*>) {
-        clz.declaredFields.forEach { XPBridge.log(it.get(null)) }
+        clz.declaredFields.forEach {
+            it.isAccessible = true
+            XPBridge.log(it.get(null))
+        }
+    }
+
+    fun printAllField(obj: Any) {
+        obj.javaClass.declaredFields.forEach {
+            it.isAccessible = true
+            XPBridge.log(it.get(obj))
+        }
     }
 
 }
