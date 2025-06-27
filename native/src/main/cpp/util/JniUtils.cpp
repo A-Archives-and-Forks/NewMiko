d31f9c59c9ec2ada501dad845e9c8301f5cbb54a
明熙 <2968447202@qq.com>
2025-06-27 12:25:45 +0800

    chore: update deps - Kotlin: 2.1.10 -> 2.1.20 - MMKV: 2.0.1 -> 2.2.2 - DexKit: 2.0.2 -> 2.0.6 - Glide: 4.13.2 -> 4.16.0
    Signed-off-by: 明熙 <2968447202@qq.com>

---
+++ native/src/main/cpp/util/JniUtils.cpp
@@ -0,0 +1,29 @@
+#include "JniUtils.h"
+
+#include <jni.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <vector>
+#include <thread>
+
+// java.lang.String 2 char*
+
+char *jstring2Char(JNIEnv *env, jstring jstr) {
+    char *rtn = nullptr;
+    jclass jString = env->FindClass("java/lang/String");
+    jstring str_utf_8 = env->NewStringUTF("UTF-8");
+    jmethodID mid = env->GetMethodID(jString, "getBytes", "(Ljava/lang/String;)[B");
+    auto barr = (jbyteArray) env->CallObjectMethod(jstr, mid, str_utf_8);
+    auto alen = env->GetArrayLength(barr);
+    jbyte *ba = env->GetByteArrayElements(barr, JNI_FALSE);
+    if (alen > 0) {
+        rtn = (char *) malloc(alen + 1);
+        memcpy(rtn, ba, alen);
+        rtn[alen] = 0;
+    }
+    env->ReleaseByteArrayElements(barr, ba, 0);
+    return rtn;
+}
---
+++ native/src/main/cpp/util/JniUtils.h
@@ -0,0 +1,15 @@
+//
+// Created by admin on 2025/6/27.
+//
+
+#ifndef MIKO_JNIUTILS_H
+#define MIKO_JNIUTILS_H
+
+
+#include <jni.h>
+
+
+char *jstring2Char(JNIEnv *env, jstring jstr);
+
+
+#endif //MIKO_JNIUTILS_H
